01cd115b8668d3127d940b7bdaeabba4865960d9
therokdaba <therokdaba@gmail.com>
2021-01-30 12:42:32 +0100

    Commit 1 - Very basic person entity setup

---
+++ main.py
@@ -0,0 +1,5 @@
+from Entities import Person
+
+if __name__ == "__main__":
+    first_person = Person.person("Rokhaya", "Fall", "female", "200", "100", "500") 
+    second_person = Person.person("Michael", "Cordero", "male", "150", "80", "250")   
